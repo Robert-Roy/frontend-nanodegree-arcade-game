96e7b8c18770bfdf0cfd451066e7ac4337b2aa6f
Robert-Roy <robertproy@live.com>
2018-03-06 19:14:55 -0600

    create skeleton of player class

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -10,21 +10,59 @@ var Enemy = function() {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-boy';
 
+    this.moveDirection = "";
+};
+Player.prototype.update = function (dt) {
+    // always reset move direction on update
+    switch(this.movedirection){
+        case "left":
+            break;
+        case "up":
+            break;
+        case "right":
+            break;
+        case "down":
+            break;
+    }
+    this.moveDirection = "";
+};
+Player.prototype.handleInput = function (inputKey) {
+    //records input to player directional movement
+    switch (inputKey) {
+        case 37:
+            this.moveDirection = "left";
+            break;
+        case 38:
+            this.moveDirection = "up";
+            break;
+        case 39:
+            this.moveDirection = "right";
+            break;
+        case 40:
+            this.moveDirection = "down";
+            break;
+    }
+};
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -34,7 +72,7 @@ Enemy.prototype.render = function() {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
